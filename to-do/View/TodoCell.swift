13c671def1399cc6b5440bb64aaab78a64f0047f
Pavel <ttt.1.12@bk.ru>
2021-07-03 21:51:11 +0300

    add properties for cell

---
+++ to-do/View/TodoCell.swift
@@ -0,0 +1,15 @@
+//
+//  TodoCell.swift
+//  to-do
+//
+//  Created by Павел Тимофеев on 03.07.2021.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    @IBOutlet var itemLabel: UILabel!
+    
+    @IBOutlet var priorityView: UIView!
+}
