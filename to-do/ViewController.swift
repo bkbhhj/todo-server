cc4453e0c45731370d8100d3ab8475b06f533a32
Pavel <ttt.1.12@bk.ru>
2021-07-03 21:02:40 +0300

    Initial Commit

---
+++ to-do/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  to-do
+//
+//  Created by Павел Тимофеев on 03.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
