488c917ede813962ea859d65504723e55a6d989f
Pavel <ttt.1.12@bk.ru>
2021-07-04 19:24:09 +0300

    parse node.js

--- to-do/Services/Network Service.swift
+++ to-do/Services/Network Service.swift
@@ -20,9 +20,30 @@ class NetworkService {
     func getTodos() {
         guard let url = URL(string: "\(URL_BASE)") else {return}
         
-        let task = session.dataTask(with: url) { (data, response, error) in
-            debugPrint(data)
-     
+        let task = session.dataTask(with: url) { data, response, error in
+
+            if let error = error {
+                debugPrint(error.localizedDescription)
+                return
+            }
+            guard let data = data, let response = response as? HTTPURLResponse else {
+                debugPrint("Ivalid data of responce!")
+                return
+            }
+            
+            do {
+                if response.statusCode == 200 {
+                    let items = try JSONDecoder().decode(Todos.self, from: data)
+                    print(items)
+                } else {
+                    let err = try JSONDecoder().decode(APIError.self, from: data)
+                }
+            } catch {
+                debugPrint(error.localizedDescription)
+            }
+
+            
+            
         }
         task.resume()
     }
